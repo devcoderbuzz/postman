8764eb5fb0683db86bf25595b0d7d9b0404cf780
DevCoder <devcoder@DevCoders-MacBook-Pro.local>
2026-01-25 20:22:22 +0800

    corrected for admin

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8g5ikl9kaig"
+    "revision": "0.vjrnks07apg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
